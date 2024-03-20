d5ecad6bfc2cc1c74646e73772ad6e2cbe87a9a1
rsumathi69 <rsumathi@yahoo.com>
2024-03-19 21:29:36 -0400

    added requirement and modified run batch file

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
+pytest -m venv env1
+env1/Scripts/activate.bat
+pip install -r requirements.txt
 pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
 pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser firefox
 pause
-
